7fe5d04841fd069e0bda029c649f4c44bc47263e
Ben Beaumont <Benhab1@aol.com>
2024-09-24 13:38:36 +0100

    Converted products into classes

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,7 @@
 import {renderOrderSummary} from './Checkout/OrderSummary.js';
 import {renderPaymentSummary} from './Checkout/PaymentSummary.js';
-import '../data/Cart-OOP.js';
-import '../data/Cart-Class.js';
+// import '../data/Cart-OOP.js';
+// import '../data/Cart-Class.js';
 
 renderOrderSummary();
 renderPaymentSummary();
